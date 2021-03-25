e4a35cbfdf066527cb7f0fe1c0c28adfff193b30
Ulugbek <abdurahmons100@gmail.com>
2021-03-25 12:47:06 +0500

    Добавлен пакет кард и тесты к нему

--- pkg/bank/card/card.go
+++ pkg/bank/card/card.go
@@ -4,80 +4,26 @@ import "bank/pkg/bank/types"
 
 
 
-func IssueCard(currency types.Currency, color string, name string) types.Card   {
-	card := types.Card{
-		ID: 1000,
-		PAN: "5058 xxxx xxxx 0001",
-		Balance: 0,
-		Currency: currency,
-		Color: color,
-		Name: name,
-		Active: true,
-	}
+
 	
-
-	return card
-	
-}
-
-
-
-func Withdraw(card *types.Card, amount types.Money)  {
-	
-	
-
-	limit := 2_000_000
-
-	if card.Active == false{
-		return 
-	}
-	
-
-	if card.Balance < amount{
-		return 
-	}
-	if amount >= types.Money(limit){
-		return 
-	}
-	card.Balance -= amount
-
-
-
-	}
-
-
-	func Deposit(card *types.Card, amount types.Money)  {
-		
-		if amount < 0{
-			return 
-		}
-
-		if !card.Active{
-			return
-		}
-
-		if amount > 5_000_000{
-			return
-		}
-
-
-
-
-		card.Balance += amount
-		
-	}
-	
-
+// тотал вычисляет общую сумму на всех картах
+// отрицательные суммы и суммы на заблокированных картах игнорируются
 
 	func Total(cards []types.Card) types.Money  {
-		var sum types.Money
+		 sum := types.Money(0)
 
 	
 
 		for _, card := range cards{
 			
-			sum +=  card.Balance
+			if !card.Active == true{
+				continue
+			}
+			if card.Balance <= 0 {
+				continue
+			}
 
+			sum += card.Balance
 		}
 
 		
--- pkg/bank/card/example_test.go
+++ pkg/bank/card/example_test.go
@@ -7,92 +7,21 @@ import (
 
 
 
-func ExampleWithdraw_positive() {
-	cards := types.Card{Balance:  2_000_000, Active: true}
-	 Withdraw(&cards, 1_000_000)
-	fmt.Println(cards.Balance)
-
-
+func ExampleTotal() {
+	cards := []types.Card{
+		{
+			Balance: 1000_00,
+			Active: true,
+		},
+		
+	}
+	 
+	fmt.Println(Total(cards))
 	// Output:
-	// 1000000
-}
-
-func ExampleDeposit_possitive()  {
-	card := types.Card{Balance: 1_000_000, Active: true}
-	Deposit(&card, 1_000_000)
-	fmt.Println(card.Balance)
-
-	// Output:
-	// 2000000
-	
-}
-
-func ExampleDeposit_inactive()  {
-	card := types.Card{Balance: 1000000, Active: false}
-	Deposit(&card, 1000)
-	fmt.Println(card.Balance)
-
-	// Output:
-	// 1000000
-	
+	// 100000
 }
 
 
-func ExampleDeposit_negative()  {
-
-	card := types.Card{Balance: -1000, Active: true}
-	Deposit(&card, 2000)
-	fmt.Println(card.Balance)
-
-	// Output:
-	// 1000
-
-	
-}
-
-
-
-
-// func ExampleWithdraw_noMoney()  {
-// 	card := types.Card{Balance: 0, Active: true}
-
-// 	 Withdraw(&card,1_000_000)
-// 	fmt.Println(card.Balance)
-
-// 	// Output:
-// 	// 0
-	
-//}
-
-// func ExampleWithdraw_inactive()  {
-// 	result := Withdraw(types.Card{Balance: 2_000_000, Active: false}, 1_000_000)
-// 	fmt.Println(result.Balance)
-
-// 	// Output:
-// 	// 2000000
-	
-// }
-
-
-// func ExampleWithdraw_limit()  {
-// 	result := Withdraw(types.Card{Balance: 10_000_000, Active: true}, 2_000_000)
-// 	fmt.Println(result.Balance)
-
-// 	// Output:
-// 	// 10000000
-
-	
-// }
-
-
-func ExampleDepositeLimit()  {
-	card := types.Card{Balance: 1_000_000, Active: true}
-	Deposit(&card, 5_000_001)
-	fmt.Println(card.Balance)
-
-	// Output: 
-	// 1000000
-}
 
 
 
